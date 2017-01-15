e8c0ac392f9951eff2a217f1beda1d0ab6af1c27
MyteDDDDDDD <elvisyuan6988@gmail.com>
2017-01-15 19:10:47 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,10 +2,10 @@
 #include <iomanip>
 using namespace std;
 
-int main(){
-	int numC; //the integer that records the number of cases 
-	int numS=0; //the integer that records the number of students in one case
-	float per=0.00; // the percentage of students whose grade is above average,initialed to be 0
+int main{
+	int numC; //records the number of cases 
+	int numS=0; //records the number of students in one case
+	float per=0; // the percentage of students whose grade is above average, initialed to be 0
 	cin>> numC;
 	for(int i=0;i<numC;i++){
 		cin>>numS;
@@ -16,19 +16,16 @@ int main(){
 			cin>>Stu[numS];
 			Sum+=Stu[numS];
 		}
+
 		float ave=Sum/numS; //calculate the average of students in one case
 
 		for(int j=0;j<numS;j++){ // calculate the average of students whose grade is above average
-			if(Stu[numS]>ave)
+			if(Stu[numS]>ave){
 			Sum++;
-			
+			}
 		}
-		
 		per=Sum/numS;
 		cout<<setprecision(3)<<per<<endl;
 	}
-	
-
-	
 	return 0;
 }
